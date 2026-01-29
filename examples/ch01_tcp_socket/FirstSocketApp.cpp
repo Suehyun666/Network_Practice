0b62232220355ad7309edb88c2df607818bd5efd
initshpark <init.sh.park@gmail.com>
2026-01-29 15:55:19 +0900

    feat(socket): Socket Initialize

---
+++ examples/ch01_tcp_socket/FirstSocketApp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <winsock2.h>
+
+using namespace std;
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+    WSADATA wsaData;
+    BYTE nMajor = 2, nMinor = 2;
+    WORD wVersionRequested = MAKEWORD(nMinor, nMajor);
+
+    int nRet = WSAStartup(wVersionRequested, &wsaData);
+    if (nRet == SOCKET_ERROR)
+    {
+        cout << "can't initialize socket" << endl;
+        return -1;
+    }
+
+    if (LOBYTE(wsaData.wVersion) != nMinor ||
+        HIBYTE(wsaData.wVersion) != nMajor)
+    {
+        cout << "window socket " << nMajor << "." << nMinor << "cant use" << endl;
+        WSACleanup();
+        return -1;
+    }
+    cout << "Initialize socket!" << endl;
+    WSACleanup();
+    return 0;
+}
